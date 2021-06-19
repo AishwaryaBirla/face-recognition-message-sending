4b37c379e6fcb5ba7bbb17770cca6d3a75634c1c
AishwaryaBirla <53036180+AishwaryaBirla@users.noreply.github.com>
2021-06-19 21:25:58 +0530

    Create private.py

---
+++ private.py
@@ -0,0 +1,7 @@
+ReceiverEmail = "email_id@gmail.com" #change This
+EmailTitle = "Hello! this is a trial" #change This
+EmailBody = "We are checking the working of code for facial recognition" #change This
+num = f"+91phone-number" #change This
+msg = "Hello this is a test msg. I created a ,model and trained it using cv2. When the camera detects me, this msg is send!!" #change This
+uname = "Senderemailid00@gmail.com" #change This
+passwd = "password" #change This
